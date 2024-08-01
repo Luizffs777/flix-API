4bafbe6c74adb94ad6afe91f73bff31115dacd42
Luiz Felipe Ferreira Santos <x510316@rede.sp>
2024-08-01 11:58:38 -0300

    adicionando CRUD movies

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView,GenreRetrieveUpdateDestoyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetriveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestoyView.as_view(), name='genre-detail-view'),
+    
     path('actors/', ActorCreateListView.as_view(), name='actor-create-list' ),
     path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+    
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-view'),
+    path('movies/<int:pk>', MovieRetriveUpdateDestroyView.as_view(), name='movie-detail-view'),
 ]
 
